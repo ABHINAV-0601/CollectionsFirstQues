e951b8460ca04f31e0ed33334ad042098ae0e8a6
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-09-01 11:00:45 +0530

    created BookImpl class, added objects in the list and sorted them and then printed them

---
+++ src/BookImpl.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class BookImpl {
+    public static void main(String[] args) {
+        Book book1 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,250.0);
+        Book book2 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,502.0);
+        Book book3 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,185.0);
+        Book book4 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,175.6);
+        Book book5 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,235.0);
+        Book book6 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,100.0);
+        Book book7 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,99.5);
+        Book book8 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,452.0);
+        Book book9 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,375.0);
+        Book book10 = new Book("Five Point Someone","Chetan Bhagat",12547874558L,135.4);
+
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+        bookList.add(book5);
+        bookList.add(book6);
+        bookList.add(book7);
+        bookList.add(book8);
+        bookList.add(book9);
+        bookList.add(book10);
+
+        for (Book element : bookList){
+            System.out.println(element);
+        }
+        // sorting logic to sort objects on the basis of any value in the element
+        for (int i = 0; i < bookList.size(); i++) {
+            for (int j = 0; j < bookList.size() - 1 - i; j++) {
+                if (bookList.get(j).getPrice() > bookList.get(j+1).getPrice()) {
+                    Book temp = bookList.get(j);
+                    bookList.set(j,bookList.get(j+1));
+                    bookList.set(j+1,temp);
+                }
+            }
+        }
+
+        /*Collections.sort(bookList);
+        bookList.sort(book1);*/
+
+        System.out.println();
+        System.out.println("after sorting");
+        System.out.println();
+        for (Book element : bookList){
+            System.out.println(element);
+        }
+    }
+}
